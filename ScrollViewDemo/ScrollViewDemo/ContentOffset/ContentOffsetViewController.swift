28a769263e43ca31b14edb9ee5c52469e71c1851
zhouming <zm18529516625@gmail.com>
2024-04-07 23:48:15 +0800

    关于

---
+++ ScrollViewDemo/ScrollViewDemo/ContentOffset/ContentOffsetViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ContentOffsetViewController.swift
+//  ScrollViewDemo
+//
+//  Created by 周明 on 2024/4/7.
+//
+
+import UIKit
+
+class ContentOffsetViewController: UIViewController, UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 88, width: UIScreen.main.bounds.size.width, height: 200))
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = .clear
+        scrollView.bounces = false
+        scrollView.delegate = self
+        view.addSubview(scrollView)
+
+        let redView = UIView(frame: CGRectMake(0, 0, UIScreen.main.bounds.size.width, 200))
+        redView.backgroundColor = .red
+        scrollView.addSubview(redView)
+        let blueView = UIView(frame: CGRectMake(0, 200, UIScreen.main.bounds.size.width, 200))
+        blueView.backgroundColor = .blue
+        scrollView.addSubview(blueView)
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("bounds:" + "\(scrollView.bounds)")
+        print("contentOffset:" + "\(scrollView.contentOffset)")
+    }
+
+
+}
