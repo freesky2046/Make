afeda8c1dfe833318bfcce3fe04ed319e6cf33dc
ming zhou <zhouming@mgtv.com>
2024-05-01 16:24:01 +0800

    描述：持久化

---
+++ PersistenceDemo/PersistenceDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  PersistenceDemo
+//
+//  Created by ming zhou on 2024/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var list:[String] = [
+        "userDefault",
+        "nskeyedarchiver",
+        "write(to:options:)"
+    ]
+    
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isPagingEnabled = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.scrollsToTop = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.estimatedRowHeight = 0
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.safeAreaLayoutGuide.layoutFrame
+        
+
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) 
+        cell.textLabel?.text = "\(list[indexPath.row])"
+        return cell
+    }
+}
+
